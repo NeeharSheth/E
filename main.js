99bd971df16028b8c8a7ed983124763ec346d10f
NeeharSheth <75029741+NeeharSheth@users.noreply.github.com>
2021-05-31 16:05:38 +0400

    Add files via upload

--- main.js
+++ main.js
@@ -10,5 +10,20 @@ recognition.onresult= function run(event){
     console.log(event);
 var content= event.results[0][0].transcript;
 document.getElementById("textbox").innerHTML= content;
+speak(content);
+camera= document.getElementById("camera");
+Webcam.attach(camera);
 }
 
+function speak(speech){
+var synth= window.speechSynthesis;
+utterThis= new SpeechSynthesisUtterance(speech);
+synth.speak(utterThis);
+}
+
+Webcam.set({
+    width: 320,
+    height: 400,
+    image_format: 'png',
+png_quality: 70
+});
