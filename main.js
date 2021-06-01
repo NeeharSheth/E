7f21856365b8c25a53cbeb2d7403ba23624d83fa
NeeharSheth <75029741+NeeharSheth@users.noreply.github.com>
2021-06-01 15:33:13 +0400

    Add files via upload

--- main.js
+++ main.js
@@ -10,9 +10,16 @@ recognition.onresult= function run(event){
     console.log(event);
 var content= event.results[0][0].transcript;
 document.getElementById("textbox").innerHTML= content;
-speak(content);
-camera= document.getElementById("camera");
-Webcam.attach(camera);
+
+if(content == "take my selfie"){
+    content = "taking your selfie in 5 seconds";
+    console.log(content);
+    speak(content);
+    setTimeout(function(){take_snapshot();},5000 );
+}
+else{
+    speak(content);
+}
 }
 
 function speak(speech){
@@ -21,9 +28,23 @@ utterThis= new SpeechSynthesisUtterance(speech);
 synth.speak(utterThis);
 }
 
+function take_snapshot(){
+    Webcam.snap(function(data_uri){document.getElementById("display_result").innerHTML = '<img id="selfie_image" src="'+data_uri+'"/>';})
+    save();
+}
+
 Webcam.set({
     width: 320,
     height: 400,
     image_format: 'png',
 png_quality: 70
 });
+camera= document.getElementById("camera");
+Webcam.attach(camera);
+
+function save(){
+link= document.getElementById("link");
+image= document.getElementById("selfie_image").src;
+link.href= image;
+link.click();
+}
